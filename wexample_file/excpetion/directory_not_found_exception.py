f3acaa1197ebb470ffef3ce51cac45f34200d3cc
weeger <romain.weeger@gmail.com>
2025-08-08 16:20:46 +0200

    Exceptions bases

---
+++ wexample_file/excpetion/directory_not_found_exception.py
@@ -0,0 +1,5 @@
+from wexample_file.excpetion.local_path_not_found_exception import LocalPathNotFoundException
+
+
+class DirectoryNotFoundException(LocalPathNotFoundException):
+    pass
---
+++ wexample_file/excpetion/file_not_found_exception.py
@@ -0,0 +1,5 @@
+from wexample_app.exception.abstract_exception import AbstractException
+
+
+class FileNotFoundException(AbstractException):
+    pass
