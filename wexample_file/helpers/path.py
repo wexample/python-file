6b995e946b39579dca30e3750280cda3ce59613d
weeger <romain.weeger@gmail.com>
2025-08-10 11:39:35 +0200

    Simplify

--- wexample_file/helpers/path.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-from wexample_file.const.types import PathOrString
-
-
-def path_wrap(path: PathOrString) -> Path:
-    if isinstance(path, str):
-        return Path(path)
-    return path
