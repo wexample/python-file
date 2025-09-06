ae3005be32021030f9554e0be612f33d8b5c2497
weeger <romain.weeger@gmail.com>
2025-09-06 11:17:13 +0200

    Mixins

---
+++ src/wexample_file/mixin/with_local_directory_mixin.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from wexample_file.mixin.with_path_mixin import WithPathMixin
+
+if TYPE_CHECKING:
+    from wexample_file.common.local_directory import LocalDirectory
+
+
+class WithLocalDirectoryMixin(WithPathMixin):
+    def get_local_directory(self) -> LocalDirectory:
+        from wexample_file.common.local_directory import LocalDirectory
+
+        return LocalDirectory(path=self.get_path())
