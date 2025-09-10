5ec30b89baa016b151751305084d024e064cde9d
weeger <romain.weeger@gmail.com>
2025-09-10 17:29:12 +0200

    Rect

--- src/wexample_file/mixin/with_local_directory_mixin.py
+++ src/wexample_file/mixin/with_local_directory_mixin.py
@@ -7,8 +7,9 @@ from wexample_file.mixin.with_path_mixin import WithPathMixin
 if TYPE_CHECKING:
     from wexample_file.common.local_directory import LocalDirectory
 
-
 from wexample_helpers.decorator.base_class import base_class
+
+
 @base_class
 class WithLocalDirectoryMixin(WithPathMixin):
     def get_local_directory(self) -> LocalDirectory:
--- src/wexample_file/mixin/with_path_mixin.py
+++ src/wexample_file/mixin/with_path_mixin.py
@@ -1,6 +1,4 @@
 from __future__ import annotations
-
-from pathlib import Path
 from typing import Any
 
 from wexample_helpers.classes.base_class import BaseClass
